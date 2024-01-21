b68e5e07bc6a97eb7e5f938a2cf504669192b0d6
zainahmed0597 <zainahmed0597@gmail.com>
2024-01-22 01:21:38 +0500

    update container

--- lib/main.dart
+++ lib/main.dart
@@ -15,31 +15,51 @@ class Practice extends StatelessWidget {
         appBar: AppBar(
           title: const Text(
             'Practice',
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              letterSpacing: 2,
+            ),
           ),
           centerTitle: true,
-          backgroundColor: Colors.pink,
+          backgroundColor: const Color.fromARGB(255, 18, 41, 85),
         ),
         backgroundColor: Colors.teal,
-        drawer: const Drawer(),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home),
+        body: Center(
+          child: Container(
+            height: 230,
+            width: double.infinity,
+            // constraints: const BoxConstraints(maxWidth: 350),
+            padding: const EdgeInsets.all(15),
+            margin: const EdgeInsets.all(30),
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+                color: Colors.purple,
+                // borderRadius: BorderRadius.all(
+                //   Radius.circular(12),
+                // ),
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.cyan,
+                    Colors.teal,
+                    Colors.teal,
+                    Colors.cyan,
+                  ],
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(1, 9),
+                    blurRadius: 25,
+                  )
+                ],
+                shape: BoxShape.rectangle),
+            child: const Text(
+              "In Flutter, the Container widget is a box model that can contain other widgets. It provides a convenient way to apply padding, margins, borders, and constraints to its child widget the Container is used to contain a text widget with a blue background. The width, height, and color properties of the Container define its dimensions and background color. The child property is used to specify the child widget inside the Container.",
+              style: TextStyle(color: Colors.white),
             ),
-            BottomNavigationBarItem(
-              label: 'Setting',
-              icon: Icon(Icons.settings),
-            ),
-          ],
+          ),
         ),
-        extendBody: false,
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {},
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
   }
