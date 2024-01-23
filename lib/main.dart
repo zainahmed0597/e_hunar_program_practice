f777f0226463bb44585c52b4fbf488728440cb79
zainahmed0597 <zainahmed0597@gmail.com>
2024-01-23 12:55:01 +0500

    container

--- lib/main.dart
+++ lib/main.dart
@@ -33,12 +33,12 @@ class Practice extends StatelessWidget {
             padding: const EdgeInsets.all(15),
             margin: const EdgeInsets.all(30),
             alignment: Alignment.center,
-            decoration: const BoxDecoration(
+            decoration:  BoxDecoration(
                 color: Colors.purple,
-                // borderRadius: BorderRadius.all(
-                //   Radius.circular(12),
-                // ),
-                gradient: LinearGradient(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(12),
+                ),
+                gradient: const LinearGradient(
                   colors: [
                     Colors.cyan,
                     Colors.teal,
@@ -46,17 +46,25 @@ class Practice extends StatelessWidget {
                     Colors.cyan,
                   ],
                 ),
-                boxShadow: [
+                boxShadow: const [
                   BoxShadow(
                     color: Colors.black26,
                     offset: Offset(1, 9),
                     blurRadius: 25,
                   )
                 ],
-                shape: BoxShape.rectangle),
+                shape: BoxShape.rectangle,
+              image: DecorationImage(
+                filterQuality: FilterQuality.high,
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.30), BlendMode.dstATop),
+                // opacity: 1.0,
+                image: const NetworkImage('https://blog.logrocket.com/wp-content/uploads/2021/04/Building-Flutter-desktop-app-tutorial-examples.png'),
+              ),
+            ),
             child: const Text(
               "In Flutter, the Container widget is a box model that can contain other widgets. It provides a convenient way to apply padding, margins, borders, and constraints to its child widget the Container is used to contain a text widget with a blue background. The width, height, and color properties of the Container define its dimensions and background color. The child property is used to specify the child widget inside the Container.",
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(color: Color.fromARGB(255, 18, 41, 85)),
             ),
           ),
         ),
